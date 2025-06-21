73c4e44ca5ca9d434445fbb77971b12e899961e7
vladyslavsoruk <sorukv@gmail.com>
2025-06-21 19:02:23 +0300

    Get All Posts Api Route logic created

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { verifyToken } from "../../utils/verifyUser.js";
 import { createPost } from "../controllers/post.controller.js";
+import { getPosts } from "../controllers/post.controller.js";
 
 const postRouter = express.Router();
 postRouter.post("/create", verifyToken, createPost);
+postRouter.get("/get", getPosts);
 // postRouter.post("/signin", signin);
 // postRouter.post("/google", google);
 
